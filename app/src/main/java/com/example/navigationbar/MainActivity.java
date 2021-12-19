62b2992d882cd627254f9edfbca6a608ded40a62
AhsanRiaz9 <bcsf18m009@pucit.edu.pk>
2021-12-19 20:16:21 +0500

    configure menu bar in main

--- app/src/main/java/com/example/navigationbar/MainActivity.java
+++ app/src/main/java/com/example/navigationbar/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.navigationbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    }
