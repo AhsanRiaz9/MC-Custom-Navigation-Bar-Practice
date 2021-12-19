a9905022c456f62dc08095aaf5419b22c973c1f0
AhsanRiaz9 <bcsf18m009@pucit.edu.pk>
2021-12-19 20:29:47 +0500

    Final Commit

--- app/src/main/java/com/example/navigationbar/MainActivity.java
+++ app/src/main/java/com/example/navigationbar/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -24,4 +25,26 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(),"Setting",Toast.LENGTH_LONG);
+                break;
+            case R.id.action_tasks:
+                Toast.makeText(getApplicationContext(),"Tasks",Toast.LENGTH_LONG);
+                break;
+            case R.id.action_signup:
+                Toast.makeText(getApplicationContext(),"Sign Up",Toast.LENGTH_LONG);
+                break;
+            case R.id.action_signin:
+                Toast.makeText(getApplicationContext(),"Sign In",Toast.LENGTH_LONG);
+                break;
+            case R.id.action_favorite:
+                Toast.makeText(getApplicationContext(),"Favorite",Toast.LENGTH_LONG);
+                break;
+        }
+        return true;
     }
+}
